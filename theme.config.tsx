4ccaf5ff1ce7204558f1ba7e0965dd8cd4b259ca
dharunkumardev <dharunkumar2511@gmail.com>
2023-12-30 13:57:29 +0530

    Pages Completed Half of

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>KPR Info Solution - Files Upload Using AWS Services</span>,
+  // project: {
+  //   link: "https://github.com/shuding/nextra-docs-template",
+  // },
+  // chat: {
+  //   link: "https://discord.com",
+  // },
+  // docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+};
+
+export default config;
