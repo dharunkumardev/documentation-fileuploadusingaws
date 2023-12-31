b4a94b046eb5d5d27fc3fb22a6fe4733c557ee94
dharunkumardev <dharunkumar2511@gmail.com>
2023-12-31 15:45:22 +0530

    Over all Documentation Completed

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,51 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
-
+import heartLogo from "./assets/heart.webp";
 const config: DocsThemeConfig = {
-  logo: <span>KPR Info Solution - Files Upload Using AWS Services</span>,
+  logo: (
+    <span>
+      <b>KPR Info Solution</b> - Files Upload and Retrieve at Fast and Secure
+      Way Using AWS Services
+    </span>
+  ),
   // project: {
   //   link: "https://github.com/shuding/nextra-docs-template",
   // },
-  // chat: {
-  //   link: "https://discord.com",
-  // },
+  chat: {
+    // link: "https://discord.com",
+  },
   // docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  // chat: <span>hello</span>
+  footer: {
+    text: (
+      <>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          Maded With
+          <img
+            src={heartLogo.src}
+            alt="Heart"
+            width={20}
+            style={{ marginLeft: "6px", marginRight: "6px" }}
+          ></img>
+          by
+          <u>
+            <a
+              href="https://github.com/dharunkpr"
+              style={{ color: "white", paddingLeft: "10px" }}
+            >
+              Dharun
+            </a>
+          </u>
+        </div>
+      </>
+    ),
+  },
 };
 
 export default config;
